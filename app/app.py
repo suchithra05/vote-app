4684b3cfd43212b2529b67af9cdc8d51cac90613
suchithra <suchithraee1999@gmail.com>
2025-07-06 21:33:26 +0530

    updated files

--- app/app.py
+++ app/app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request
 import redis
 
-##app = Flask(__name__)
-app= Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates')
 r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def index():
     votes_dog = int(r.get('dog') or 0)
     votes_cat = int(r.get('cat') or 0)
-    return render_template("index.html", dog=votes_dog, cat=votes_cat)
+    votes_buffalo = int(r.get('buffalo') or 0)
+    return render_template("index.html", dog=votes_dog, cat=votes_cat, buffalo=votes_buffalo)
 
 @app.route('/vote', methods=['POST'])
 def vote():
     vote = request.form.get('vote')
-    if vote == 'dog':
-        r.incr('dog')
-    elif vote == 'cat':
-        r.incr('cat')
+    if vote in ['dog', 'cat', 'buffalo']:
+        r.incr(vote)
     return ('', 204)
 
 if __name__ == "__main__":
